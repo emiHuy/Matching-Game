dea3b9299a7d1201ea9ae08c949b459646d9ab73
ehuyn <163863983+ehuyn@users.noreply.github.com>
2024-04-20 16:39:30 -0400

    made instructions

--- src/instructionsPage.java
+++ src/instructionsPage.java
@@ -5,6 +5,7 @@ public class instructionsPage extends JFrame implements ActionListener{
     private JFrame instructionsFrame;
     private JPanel instructionPanel;
     private JButton backButton;
+    private JTextArea instructionText;
 
     public instructionsPage(){
         instructionSetup();
@@ -14,11 +15,29 @@ public class instructionsPage extends JFrame implements ActionListener{
         instructionsFrame = new JFrame();
         instructionsFrame.add(instructionPanel);
         instructionsFrame.setTitle("Card Matching Game Instructions");
-        instructionsFrame.setSize(1500, 700);
+        instructionsFrame.setSize(1800, 950);
         instructionsFrame.setLocationRelativeTo(null);
         instructionsFrame.setVisible(true);
         instructionsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         backButton.addActionListener(this);
+
+        instructionText.setText("Welcome to the Card Matching Game!\n\n" +
+                "Objective:\n" +
+                "The goal of this game is to match all pairs of cards on the board.\n\n" +
+                "How to Play:\n" +
+                "1. Click on two cards to reveal their icons.\n" +
+                "2. If the icons of the two selected cards match, you will earn 100 points.\n" +
+                "   If the icons do not match, you will lose 10 points. However, your score will never go below 0.\n" +
+                "3. Click the 'Continue' button in the match results pop-up message to continue the game.\n" +
+                "         - matched cards will leave the screen\n" +
+                "         -mismatched cards will flip back over\n" +
+                "4. Continue matching cards until all cards are matched.\n" +
+                "5. Once the game is finished, enter your name\n\n" +
+                "Tips:\n" +
+                "- Remember the position of the cards to make successful matches\n" +
+                "- Keep track of your score - try to maximize it by minimizing failed attempts.\n" +
+                "         - The max score achievable is 600 points.\n\n" +
+                "Enjoy the game and have fun!");
 
         instructionsFrame.addWindowListener(new WindowAdapter() {
             @Override
